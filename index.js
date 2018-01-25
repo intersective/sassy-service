73e1403efedd96181e09959512d049dde168e751
Shawn <shawnm0705@gmail.com>
2018-01-25 17:53:22 +1100

    test

--- index.js
+++ index.js
@@ -13,38 +13,47 @@ const runner = require('./runner');
 const port = process.env.PORT || 8080;
 const router = express.Router();
 
-router.get('/*.css', function(req, res) {
-  if (!req.query.p) return res.status(404).end();
+const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
+app.use(awsServerlessExpressMiddleware.eventContext())
+app.get('/', (req, res) => {
+  res.render('index', {
+    apiUrl: req.apiGateway ? `https://${req.apiGateway.event.headers.Host}/${req.apiGateway.event.requestContext.stage}` : 'http://localhost:3000'
+  })
+})
+module.exports = app;
 
-  let cssFilePath = path.resolve(
-    path.join(config.directory.build, req.query.p, 'build', req.path)
-  );
-  res.sendFile(cssFilePath, {}, (err) => {
-    if (err) return res.status(404).end();
-  });
-});
+// router.get('/*.css', function(req, res) {
+//   if (!req.query.p) return res.status(404).end();
 
-router.get('/**/**.*', function(req, res) {
-  let target = path.resolve(path.join('./static', req.path));
-  res.sendFile(target, {}, (err) => {
-    if (err) return res.status(404).end();
-  });
-});
+//   let cssFilePath = path.resolve(
+//     path.join(config.directory.build, req.query.p, 'build', req.path)
+//   );
+//   res.sendFile(cssFilePath, {}, (err) => {
+//     if (err) return res.status(404).end();
+//   });
+// });
 
-router.get('/', function(req, res) {
-  res.status(200).json({
-    status: 'okay',
-    port: port
-  });
-});
+// router.get('/**/**.*', function(req, res) {
+//   let target = path.resolve(path.join('./static', req.path));
+//   res.sendFile(target, {}, (err) => {
+//     if (err) return res.status(404).end();
+//   });
+// });
 
-app.use('/', router);
+// router.get('/', function(req, res) {
+//   res.status(200).json({
+//     status: 'okay',
+//     port: port
+//   });
+// });
 
-if (app.get('env') === 'local') {
-  const server = http.createServer(app).listen(port, () => {
-    console.log('Server listening on port', port);
-  });
-} else {
-  // use API Gateway to handle the web requests instead of TCP port
-  module.exports = app;
-}
+// app.use('/', router);
+
+// if (app.get('env') === 'local') {
+//   const server = http.createServer(app).listen(port, () => {
+//     console.log('Server listening on port', port);
+//   });
+// } else {
+//   // use API Gateway to handle the web requests instead of TCP port
+//   module.exports = app;
+// }
