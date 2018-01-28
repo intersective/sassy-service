03a38cf55d1266ec5e01b9ae33eda2734b828e2b
Shawn <shawnm0705@gmail.com>
2018-01-29 10:07:59 +1100

    Revert "test"
    This reverts commit 1ef93f07abaa6fb3824bb51957ae8ab7e3373f11.

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 'use strict'
 const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const compression = require('compression')
+const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
 const app = express()
 
+app.set('view engine', 'pug')
+app.use(compression())
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(awsServerlessExpressMiddleware.eventContext())
+
 app.get('/', (req, res) => {
-    res.sendFile(`${__dirname}/index.html`)
+  res.json(req.apiGateway.event)
 })
 
-// app.listen(3000) // <-- comment this line out from your app
+// The aws-serverless-express library creates a server and listens on a Unix
+// Domain Socket for you, so you can remove the usual call to app.listen.
+// app.listen(3000)
 
-module.exports = app // export your app so aws-serverless-express can use it
+// Export your express server so you can import it in the lambda function.
+module.exports = app
