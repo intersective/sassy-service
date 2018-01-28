8caf5441caba1aeb066c793d6c6e909964f82ef3
Shawn <shawnm0705@gmail.com>
2018-01-29 09:06:59 +1100

    test

--- index.js
+++ index.js
@@ -1,60 +1,27 @@
-const express = require('express');
-const app = express();
-
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
-const cors = require('cors');
-app.use(cors());
-
-const config = require('./config');
-const runner = require('./runner');
-
-// const port = process.env.PORT || 8080;
-// const router = express.Router();
-
+'use strict'
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const compression = require('compression')
 const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
+const app = express()
+
+app.set('view engine', 'pug')
+app.use(compression())
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(awsServerlessExpressMiddleware.eventContext())
+
 app.get('/', (req, res) => {
-  res.status(200).json({
-    status: 'okay',
-    port: port
-  });
+  res.render('index', {
+    apiUrl: req.apiGateway ? `https://${req.apiGateway.event.headers.Host}/${req.apiGateway.event.requestContext.stage}` : 'http://localhost:3000'
+  })
 })
-module.exports = app;
 
-// router.get('/*.css', function(req, res) {
-//   if (!req.query.p) return res.status(404).end();
+// The aws-serverless-express library creates a server and listens on a Unix
+// Domain Socket for you, so you can remove the usual call to app.listen.
+// app.listen(3000)
 
-//   let cssFilePath = path.resolve(
-//     path.join(config.directory.build, req.query.p, 'build', req.path)
-//   );
-//   res.sendFile(cssFilePath, {}, (err) => {
-//     if (err) return res.status(404).end();
-//   });
-// });
-
-// router.get('/**/**.*', function(req, res) {
-//   let target = path.resolve(path.join('./static', req.path));
-//   res.sendFile(target, {}, (err) => {
-//     if (err) return res.status(404).end();
-//   });
-// });
-
-// router.get('/', function(req, res) {
-//   res.status(200).json({
-//     status: 'okay',
-//     port: port
-//   });
-// });
-
-// app.use('/', router);
-
-// if (app.get('env') === 'local') {
-//   const server = http.createServer(app).listen(port, () => {
-//     console.log('Server listening on port', port);
-//   });
-// } else {
-//   // use API Gateway to handle the web requests instead of TCP port
-//   module.exports = app;
-// }
+// Export your express server so you can import it in the lambda function.
+module.exports = app
