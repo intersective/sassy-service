4a6ee9e81d2e7d07af2dbb6190b52753f2b3ca9e
Shawn <shawnm0705@gmail.com>
2018-01-29 17:51:44 +1100

    test

--- app.js
+++ app.js
@@ -13,10 +13,28 @@ app.use(cors())
 app.use(awsServerlessExpressMiddleware.eventContext())
 
 app.get('/sassy-front-end', (req, res) => {
-  res.json(req.apiGateway.event)
-  // res.sendFile(`${__dirname}/test.css`)
-})
+	// use css from program id
+	if (req.apiGateway.event.queryStringParameters.p) {
+		var programId = req.apiGateway.event.queryStringParameters.p;
+		return res.sendFile(`${__dirname}/static/programs/${programId}/practera.css`, {}, (err) => {
+	    if (err) return res.status(404).end();
+	  });
+	}
 
+	// use css from experience id
+	if (req.apiGateway.event.queryStringParameters.e) {
+		var experienceId = req.apiGateway.event.queryStringParameters.e;
+		return res.sendFile(`${__dirname}/static/experiences/${experienceId}/practera.css`, {}, (err) => {
+	    if (err) return res.status(404).end();
+	  });
+	}
+
+	// use default css
+	if (!req.apiGateway.event.queryStringParameters) {
+		return res.sendFile(`${__dirname}/static/practera.css`)	
+	}
+  
+})
 
 // The aws-serverless-express library creates a server and listens on a Unix
 // Domain Socket for you, so you can remove the usual call to app.listen.
