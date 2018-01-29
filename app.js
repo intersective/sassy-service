7d4a858663ec6989a137e1935375794abfaf4091
Shawn <shawnm0705@gmail.com>
2018-01-29 18:15:38 +1100

    test

--- app.js
+++ app.js
@@ -13,24 +13,25 @@ app.use(cors())
 app.use(awsServerlessExpressMiddleware.eventContext())
 
 app.get('/sassy-front-end', (req, res) => {
+	var query = req.apiGateway.event.queryStringParameters;
 	// use css from program id
-	// if (req.apiGateway.event.queryStringParameters.hasOwnProperty("p") {
-	// 	var programId = req.apiGateway.event.queryStringParameters.p;
-	// 	return res.sendFile(`${__dirname}/static/programs/${programId}/practera.css`, {}, (err) => {
-	//     if (err) return res.status(404).end();
-	//   });
-	// }
+	if (query.hasOwnProperty("p") {
+		var programId = query.p;
+		return res.sendFile(`${__dirname}/static/programs/${programId}/practera.css`, {}, (err) => {
+	    if (err) return res.status(404).end();
+	  });
+	}
 
-	// // use css from experience id
-	// if (req.apiGateway.event.queryStringParameters.hasOwnProperty("e")) {
-	// 	var experienceId = req.apiGateway.event.queryStringParameters.e;
-	// 	return res.sendFile(`${__dirname}/static/experiences/${experienceId}/practera.css`, {}, (err) => {
-	//     if (err) return res.status(404).end();
-	//   });
-	// }
+	// use css from experience id
+	if (query.hasOwnProperty("e")) {
+		var experienceId = query.e;
+		return res.sendFile(`${__dirname}/static/experiences/${experienceId}/practera.css`, {}, (err) => {
+	    if (err) return res.status(404).end();
+	  });
+	}
 
 	// use default css
-	if (!req.apiGateway.event.queryStringParameters) {
+	if (!query) {
 		return res.sendFile(`${__dirname}/static/practera.css`)	
 	}
   
