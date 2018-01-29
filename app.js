3ab19aa3351a010e8ff27bb55b31f6af5b4455d2
Shawn <shawnm0705@gmail.com>
2018-01-29 16:42:47 +1100

    test

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict'
 const express = require('express')
-const bodyParser = require('body-parser')
+// const bodyParser = require('body-parser')
 const cors = require('cors')
 const compression = require('compression')
 const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
@@ -8,8 +8,8 @@ const app = express()
 
 app.use(compression())
 app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({ extended: true }))
 app.use(awsServerlessExpressMiddleware.eventContext())
 
 app.get('/sassy-front-end', (req, res) => {
