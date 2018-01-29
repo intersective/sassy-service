12fda35f84615153a72919a2970d1f8917caadb7
Shawn <shawnm0705@gmail.com>
2018-01-29 13:21:00 +1100

    test

--- lambda.js
+++ lambda.js
@@ -2,4 +2,9 @@
 const awsServerlessExpress = require('aws-serverless-express')
 const app = require('./index')
 const server = awsServerlessExpress.createServer(app)
-exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context)
+// exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context)
+exports.handler = (event, context, callback) => {
+	console.log('event:', event);
+	console.log('context:', context);
+	callback(null, "Hello!");
+}
