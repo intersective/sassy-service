6463598329818be71cd2e1f0d4f2fb632e4235f4
Shawn <shawnm0705@gmail.com>
2018-01-30 14:24:21 +1100

    change to file system

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
     source: './source',
     build: './build',
     static: './static',
-    // live: './static/css'
-    live: 'https://s3-ap-southeast-2.amazonaws.com/css.practera.com'
+    live: './static/css'
+    // live: 'https://s3-ap-southeast-2.amazonaws.com/css.practera.com'
   }
 }
